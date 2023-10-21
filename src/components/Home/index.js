6dd71fabaec6e2c83087a56b0a13bda1646b0c0d
micho132 <michelbouchahine132@gmail.com>
2023-10-21 13:13:08 +0300

    second commit (navbar component)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import Navbar from '../Navbar';
+import './index.css';
+import Posts from '../Posts';
 
 function Home() {
     return (
-      <>
+    <>
         <Navbar></Navbar>
-      </>
+        <div className='post-class'>
+            <Posts></Posts>
+            <Posts></Posts>
+            <Posts></Posts>
+       </div>
+    </>
     );
 }
 
